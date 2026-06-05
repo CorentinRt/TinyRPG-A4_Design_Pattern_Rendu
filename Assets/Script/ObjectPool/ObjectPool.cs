edf230ae544e07e78f6bf7434ea4ab1ac446acab
LucLenne <luc.lenne@outlook.fr>
2026-06-05 23:00:33 +0200

    [ADD] generic class pooling system

---
+++ Assets/Script/ObjectPool/ObjectPool.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+public class ObjectPool<T> where T : class
+{
+    private Stack<T> _pool = new Stack<T>();
+
+    private Func<T> _createFunc;
+    private Action<T> _actionOnGet;
+    private Action<T> _actionOnRelease;
+
+    public ObjectPool(Func<T> createFunc, Action<T> actionOnGet = null, Action<T> actionOnRelease = null)
+    {
+        _createFunc = createFunc;
+        _actionOnGet = actionOnGet;
+        _actionOnRelease = actionOnRelease;
+    }
+
+    public T Get()
+    {
+        T item;
+
+        if (_pool.Count > 0)
+            item = _pool.Pop();
+        else
+            item = _createFunc();
+
+        _actionOnGet?.Invoke(item);
+
+        return item;
+    }
+
+    public void Release(T item)
+    {
+        _actionOnRelease?.Invoke(item);
+        _pool.Push(item);
+    }
+}
