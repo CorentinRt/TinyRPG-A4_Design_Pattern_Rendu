ed225be6ffcb0d74dd2811cb4ba896b2e81812e7
Corentinrmt <corentin.remot@gmail.com>
2026-06-05 11:49:42 +0200

    [UPDATE] rewind attack player (only visual since health are already rewinded)

--- Assets/Script/Commands/Attacks/Command_Attack.cs
+++ Assets/Script/Commands/Attacks/Command_Attack.cs
@@ -2,13 +2,42 @@ using UnityEngine;
 
 public class Command_Attack : Command
 {
+    #region Fields
+    private AttackBehaviourBase _attack;
+
+    private Rigidbody _rb;
+
+    private AttackParams _attackParam;
+
+    private Vector3 _impulseDir;
+
+    private int _attackIndex;
+
+    #endregion
+
+
+    public Command_Attack(AttackBehaviourBase attack, Rigidbody rb, AttackParams attackParam, int attackIndex, Vector3 ImpulseDir)
+    {
+        _attack = attack;
+     
+        _rb = rb;
+
+        _attackParam = attackParam;
+
+        _attackIndex = attackIndex;
+    }
+
     public override void Do()
     {
-        throw new System.NotImplementedException();
+        _rb.AddForce(_impulseDir * _attackParam.ImpulseForce, ForceMode.Impulse);
+
+        _attack.NotifyAttackOnly(_attackParam, _attackIndex);   // only notify here to avoid collision to trigger damages again but to keep animation attack
     }
 
     public override void Undo()
     {
-        throw new System.NotImplementedException();
+        _rb.AddForce(-_impulseDir * _attackParam.ImpulseForce, ForceMode.Impulse);
+
+        _attack.NotifyAttackOnly(_attackParam, _attackIndex);   // only notify event here to avoid collision to trigger damages again but to keep animation attack
     }
 }
--- Assets/Script/Player/PlayerAttackBehaviour.cs
+++ Assets/Script/Player/PlayerAttackBehaviour.cs
@@ -57,6 +57,7 @@ public class PlayerAttackBehaviour : AttackBehaviourBase
         return _isAttacking;
     }
 
+
     public override void TriggerNewAttack(AttackParams attackParams, int index)
     {
         if (IsInCooldown() || IsAttacking() || !IsAttackEnabled())
@@ -69,6 +70,13 @@ public class PlayerAttackBehaviour : AttackBehaviourBase
         _lastAttackTime = Time.time;
 
         StartAttackDurationCoroutine(attackParams);
+
+        Command_Attack commandAttack = new Command_Attack(this, _rb, attackParams, index, _anchorDirection.forward);
+
+        if (PlayerCommandRewind.Exist)
+        {
+            PlayerCommandRewind.Instance.RegisterCommand(commandAttack);
+        }
     }
 
     private void ApplyImpuleForce(float impulseForce)
